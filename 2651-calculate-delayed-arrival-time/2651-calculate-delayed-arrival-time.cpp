eacaf823958b072ca7f7fe9a950b41ed6e34e67b
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2024-01-03 23:26:56 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (73.79%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int time = arrivalTime + delayedTime;
+        if(time >= 24){
+            time -= 24;
+        }
+        return time ;
+    }
+};
