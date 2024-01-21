9548f2195bb504bc2b442bf7466e306e32f7a2a1
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2024-01-21 15:06:45 +0530

    Time: 11 ms (18.68%), Space: 13.8 MB (5.38%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int, int> mp;
+        
+        for(int& num : nums){
+            mp[num]++;
+        }
+       
+        // sorting the nums array based on freq using lambda function
+        sort(begin(nums), end(nums),[&mp](int& a, int& b){
+            return (mp[a] == mp[b]) ? a > b : mp[a] < mp[b];
+        });
+        
+        return nums;
+    }
+};
