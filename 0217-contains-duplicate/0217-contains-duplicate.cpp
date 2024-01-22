6e3cb88a17901f35b23697acf12e38f69b942655
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2024-01-22 10:10:45 +0530

    Time: 113 ms (38.23%), Space: 75.3 MB (7.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        
+        for(int& num : nums){
+            mp[num]++;
+        }
+        
+        for(auto& [f,s] : mp){
+            if(s >= 2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
