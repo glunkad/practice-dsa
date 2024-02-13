e6e0db6198204e08ff256f071178962fc91159d0
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2024-02-13 11:42:54 +0530

    Time: 0 ms (100.00%), Space: 23.3 MB (29.92%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -4,11 +4,26 @@ public:
         int count = 0;
         int sz = size(nums);
         
-        for(int i = 0; i < sz; i++){
-            for(int j = i + 1; j < sz; j++){
-                if(nums[i]+nums[j] < target){
-                    count++;
-                }
+        sort(begin(nums),end(nums));
+        
+        // for(int i = 0; i < sz; i++){
+        //     for(int j = i + 1; j < sz; j++){
+        //         if(nums[i]+nums[j] < target){
+        //             count++;
+        //         }
+        //     }
+        // }
+        
+        int left = 0;
+        int right = sz-1;
+        
+        for(;left < right;){
+            if(nums[left]+nums[right] < target){
+                count += right - left;
+                left++;
+            }
+            else{
+                right--;
             }
         }
         
