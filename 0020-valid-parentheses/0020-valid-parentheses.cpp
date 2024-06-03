cf0a5108ec4c07e26e41b6137ea23de82251b777
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2024-06-03 15:25:34 +0530

    Time: 2 ms (43.53%), Space: 8.1 MB (16.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
+        unordered_map<char,char> mp = {
+            {'(',')'},
+            {'[',']'},
+            {'{','}'},
+        };
         stack<char> st;
+        
         for(char& ch : s){
-            if(ch == '{' || ch == '(' || ch == '['){
+            if(ch == '(' || ch == '[' || ch == '{'){
                 st.push(ch);
             }
-            else if(st.size() > 0 && 
-                    (
-                        (ch == ']' && st.top() == '[') ||
-                        (ch == '}' && st.top() == '{') ||
-                        (ch == ')' && st.top() == '(')
-                    )){
+            else if(st.size() > 0 && mp[st.top()] == ch){
                 st.pop();
             }
             else{
@@ -19,6 +20,5 @@ public:
             }
         }
         return st.size() == 0;
-        
     }
 };
